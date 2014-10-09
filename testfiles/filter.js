cba3075372628f8ce9dbeeea3fd45bdcfbb60b1b
redking <cormac.flynn@gmail.com>
2014-10-09 16:59:59 +0200

    Test files for PO filter

--- testfiles/filter.js
+++ testfiles/filter.js
@@ -1,14 +1,24 @@
-module.exports = function filter(gt, next) {
-	var clientSideSource = 'msgid.php';
+/**
+ * An example filter; it accepts the gettext object as its first argument, and
+ * should invoke the callback with an error object and the modified translation
+ * table.
+ *
+ * @param gt
+ * @param callback
+ */
+module.exports = function filter(gt, callback) {
+	var clientSideSource = '/frontend/';
+	var err;
 
 	gt.listKeys("et").forEach(function(key) {
 		var comment = gt.getComment("et", "", key);
 		if (comment) {
+			console.log(comment);
 			if (comment.code && comment.code.indexOf(clientSideSource) === -1) {
 				gt.deleteTranslation("et", "", key);
 			}
 		}
 	});
 
-	return gt;
+	callback(err, gt._domains[gt._textdomain]._translationTable);
 };
