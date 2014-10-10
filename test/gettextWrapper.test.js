202f062686c7137703dc09f6171b8964e129f61d
redking <cormac.flynn@gmail.com>
2014-10-10 16:39:16 +0200

    Ignore all test artifacts

---
+++ test/gettextWrapper.test.js
@@ -0,0 +1,10 @@
+var chai = require('chai')
+  , expect = chai.expect;
+
+describe('JSON flattening', function() {
+
+	it('should do something', function() {
+		expect(true).to.equal(true);
+	});
+
+});
